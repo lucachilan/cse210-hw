2cc3e5a27fefbc130313b08b078863f5cfe295a3
Luca Chilan <lucachilan@gmail.com>
2026-01-07 02:17:35 -0300

    ended exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+        Console.WriteLine ("Welcome to Guess my Number");
+
+        while (response != "no")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            bool guessed = false;
+            do
+            {
+                Console.Write("What's your guess? ");
+                string guessInput = Console.ReadLine();
+                int guess = int.Parse(guessInput);   
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                    guessed = true;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    guessed = false;
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                    guessed = false;
+                }
+            } while (guessed == false);
+            
+            Console.WriteLine("Do you want to continue? ");
+            response =  Console.ReadLine();
+        }
+        Console.WriteLine("\nHave a nice day then!");
     }
 }
