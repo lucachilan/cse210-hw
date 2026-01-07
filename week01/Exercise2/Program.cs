1011a980915ca8980c7ba480e4bb774d91687275
Luca Chilan <lucachilan@gmail.com>
2026-01-07 01:31:14 -0300

    ended exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What was the grade? ");
+        string inputFromUser = Console.ReadLine();
+        int grade = int.Parse(inputFromUser);
+        string letterGrade;
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        int remainder = grade % 10;
+        string sign;
+        
+        if (remainder >=7 )
+        {
+            sign = "+";
+            if (letterGrade == "A")
+            {
+                sign = "";
+            }
+        }
+        else if (remainder < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+        if (letterGrade == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"The grade is {letterGrade}{sign}");
+
     }
 }
